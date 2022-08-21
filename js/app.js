daf5641dd2c8ea477431c40de7da798c69f2077f
m7real <masayeedreal@gmail.com>
2022-08-21 12:41:08 +0600

    Errors Handled

--- js/app.js
+++ js/app.js
@@ -13,23 +13,51 @@ function setTextElimentValueById(id, value) {
   const element = document.getElementById(id);
   element.innerText = value;
 }
+function inputValidation() {
+  const inputFields = document.getElementsByClassName("input");
+  for (const inputField of inputFields) {
+    const inputValue = inputField.value;
+    if (isNaN(inputValue)) {
+      alert("Input Numbers Only");
+      return true;
+    } else if (inputValue < 0) {
+      alert("Invalid Input");
+      return true;
+    } else if (inputValue === "") {
+      alert("Input Field Can not Be Empty");
+      return true;
+    }
+  }
+}
 
 document.getElementById("btn-calc").addEventListener("click", function () {
+  if (inputValidation()) {
+    return;
+  }
   const income = getFieldValueById("income");
   const foodExpense = getFieldValueById("food");
   const rentExpense = getFieldValueById("rent");
   const clothesExpense = getFieldValueById("clothes");
   const expenseTotal = foodExpense + rentExpense + clothesExpense;
-  setTextElimentValueById("expense-total", expenseTotal);
   const balance = income - expenseTotal;
+
+  if (balance < 0) {
+    alert("Your Expense Can Not Be More Than Your Income");
+    return;
+  }
+
+  setTextElimentValueById("expense-total", expenseTotal);
   setTextElimentValueById("balance", balance);
   setTextElimentValueById("remaining-balance", balance);
 });
 
 document.getElementById("btn-save").addEventListener("click", function () {
   const savingRate = getFieldValueById("save-field");
+  if (isNaN(savingRate) || savingRate === "" || savingRate < 0 || savingRate > 100) {
+    alert("Invalid Save Percentage");
+    return;
+  }
   const currentBalance = getTextElimentValueById("balance");
-  // const savingAmount = parseFloat((currentBalance * (savingRate / 100)).toFixed(2));
   const savingAmount = currentBalance * (savingRate / 100);
   setTextElimentValueById("saving-amount", savingAmount);
   const remainingBalance = currentBalance - savingAmount;
